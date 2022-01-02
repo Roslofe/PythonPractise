763e232ad5d6c94ec4b4701d481a4d39215a92be
roslofe1 <ella.roslof@aalto.fi>
2022-01-02 11:37:47 +0200

    Added simple information gathering excercise.

---
+++ Chapter2/personalInformation.py
@@ -0,0 +1,7 @@
+# Instructions: Write a program displaying your name, address, phone number and major
+name = input("Enter your name: ")
+address = input("Enter your address: ")
+phone = input("Enter your phone number: ")
+major = input("Enter your major: ")
+
+print("Your information: \nName: ", name, "\nAddress: ", address, "\nPhone number: ", phone, "\nCollege major: ", major)
